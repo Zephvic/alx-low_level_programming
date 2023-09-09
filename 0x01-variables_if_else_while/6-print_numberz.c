94994590a332a01f634dfbe39905ac1b08fad91d
zephvic <zephvic@gmail.com>
2023-09-08 17:12:31 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,14 +6,13 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int num;
 
-	for(num = 0; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	
+
 	putchar('\n');
 
 	return (0);
