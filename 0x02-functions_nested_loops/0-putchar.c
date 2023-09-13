6af16a2026568a3fe764892ae219c6bb74c2742d
zephvic <zephvic@gmail.com>
2023-09-12 17:11:34 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
+
 /**
- * main - function prints text as output.
+ * main -function prints text as output,
  *
  * Return: Always 0
  */
@@ -15,8 +16,7 @@ int main(void)
         _putchar('c');
         _putchar('h');
 	_putchar('a');
-        _putchar('r');
+	_putchar('r');
         _putchar('\n');
-
 	return (0);
 }
