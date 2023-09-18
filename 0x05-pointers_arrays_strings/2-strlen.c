d5893f91d8ba2bf159dcd4c8056119131548d75b
zephvic <zephvic@gmail.com>
2023-09-18 14:23:32 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - function that return the length of a string
- * 
+ * code by Zeph Victory
  * @s: char to check
  * Description: this will return the length of a string
  * Return: 0 is success
