994fe6923410b0dc316050728cfe6bd49c3787c3
zephvic <zephvic@gmail.com>
2023-09-25 15:52:13 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,15 +3,12 @@
 /**
  * _memset - prints a function that fills the first @n bytes of the memory area pointed
  * to by @s with the constant byte @b
- *
  * @n: bytes of the memory area pointed to by @s
- *
  * @s: with the constant byte @b
- *
  * @b: memory area pointer
- *
  * Return: a pointer to the memory area @s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
