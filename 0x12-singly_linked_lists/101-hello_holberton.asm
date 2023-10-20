fdcb27f0d3ab9f0d3968ad316e704d163749aa58
zephvic <zephvic@gmail.com>
2023-10-19 19:38:37 -0700

    101-hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 10, 0 ; The string to be printed (including a newline character)
-
-section .text
-    global _start
-
-_start:
-    ; Call printf
-    push rbp
-    mov rdi, hello
-    call printf
-    pop rbp
-
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit (60)
-    xor rdi, rdi        ; status: 0
-    syscall
-
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
