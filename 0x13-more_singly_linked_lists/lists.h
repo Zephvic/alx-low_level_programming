072bc998a5307767a21c39c8a987f067eabae72d
zephvic <zephvic@gmail.com>
2023-10-24 13:04:53 -0700

    header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,6 +8,9 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -19,6 +22,8 @@ typedef struct listint_s
  * struct listp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
  */
 typedef struct listp_s
 {
@@ -26,79 +31,20 @@ typedef struct listp_s
 	struct listp_s *next;
 } listp_t;
 
-/**
- * print_listint - Prints all the elements of a listint_t list.
- */
 size_t print_listint(const listint_t *h);
-
-/**
- * listint_len - Counts the number of nodes in a listint_t list.
- */
 size_t listint_len(const listint_t *h);
-
-/**
- * add_nodeint - Adds a new node at the beginning of a listint_t list.
- */
 listint_t *add_nodeint(listint_t **head, const int n);
-
-/**
- * add_nodeint_end - Adds a new node at the end of a listint_t list.
- */
 listint_t *add_nodeint_end(listint_t **head, const int n);
-
-/**
- * free_listint - Frees a listint_t list.
- */
 void free_listint(listint_t *head);
-
-/**
- * free_listint2 - Frees a listint_t list and sets the head to NULL.
- */
 void free_listint2(listint_t **head);
-
-/**
- * pop_listint - Removes the head node of a listint_t list.
- */
 int pop_listint(listint_t **head);
-
-/**
- * get_nodeint_at_index - Returns the nth node of a listint_t list.
- */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-
-/**
- * sum_listint - Returns the sum of all the data (n) of a listint_t list.
- */
 int sum_listint(listint_t *head);
-
-/**
- * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t list.
- */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-
-/**
- * delete_nodeint_at_index - Deletes a node at a given index in a listint_t list.
- */
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
-/**
- * reverse_listint - Reverses a listint_t list.
- */
 listint_t *reverse_listint(listint_t **head);
-
-/**
- * print_listint_safe - Prints all the elements of a listint_t list safely.
- */
 size_t print_listint_safe(const listint_t *head);
-
-/**
- * free_listint_safe - Frees a listint_t list safely.
- */
 size_t free_listint_safe(listint_t **h);
-
-/**
- * find_listint_loop - Finds the loop in a listint_t list.
- */
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
