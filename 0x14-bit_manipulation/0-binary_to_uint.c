3850a4556c4a6ed87a7406d01dc56757fb5fc88e
zephvic <zephvic@gmail.com>
2023-10-26 18:01:40 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- *  * binary_to_uint - Converts a binary number to an unsigned int.
- *   * @b: Pointer to a string of 0 and 1 characters.
- *    * Return: The converted number, or 0 if:
- *     * - There is one or more chars in the string `b` that is not '0' or '1'.
- *      * - `b` is NULL.
- *       */
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string of 0 and 1 characters.
+ * Return: The converted number, or 0 if:
+ * - There is one or more chars in the string `b` that is not '0' or '1'.
+ * - `b` is NULL.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-		unsigned int result = 0;
-			unsigned int base = 1;
-				int i = 0;
+	unsigned int result = 0;
+	unsigned int base = 1;
+	int i = 0;
 
-					if (b == NULL)
-								return (0);
+	if (b == NULL)
+	return (0);
 
-						while (b[i])
-								{
-											if (b[i] != '0' && b[i] != '1')
-															return (0);
+	while (b[i])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
-													i++;
-														}
+		i++;
+	}
 
-							i = i - 1;
-								while (i >= 0)
-										{
-													if (b[i] == '1')
-																	result += base;
+	i = i - 1;
+	while (i >= 0)
+	{
+		if (b[i] == '1')
+			result += base;
 
-															base *= 2;
-																	i--;
-																		}
+		base *= 2;
+		i--;
+	}
 
-									return (result);
+	return (result);
 }
 
