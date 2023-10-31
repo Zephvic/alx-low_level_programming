d3b9645c47578bd414ebda458117397cc7bce0cb
zephvic <zephvic@gmail.com>
2023-10-31 06:34:18 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,43 +12,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
 		return (0);
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	char *buf = malloc(letters);
-
-	if (buf == NULL)
-	{
-		close(fd);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	ssize_t bytes_read = read(fd, buf, letters);
-
-	if (bytes_read == -1)
-	{
-		close(fd);
-		free(buf);
-		return (0);
-	}
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-
-	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
-	{
-		close(fd);
-		free(buf);
-		return (0);
-	}
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
+
 	free(buf);
 
-	return (bytes_written);
+	return (nwr);
 }
 
